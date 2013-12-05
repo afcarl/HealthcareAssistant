5d9083486951a9e627def149e2e83e4f1378d4f9
sebastianGehrmann <sebastian.gehrmann@nordakademie.de>
2013-12-04 23:59:13 -0500

    Pair class

--- models.py
+++ models.py
@@ -88,6 +88,43 @@ class Conflict():
     def __repr__(self):
         return "Conf(" + str(self.body_function) + ", " + str(self.conflicting_treatments) + ", " + str(self.score) + ")"
 
+'''
+Created on Oct 20, 2013
+
+@author: Ofra
+'''
+
+class Pair(object):
+    '''
+    A utility class to represent pairs (ordering of the objects in the pair does not matter). It is used to represent mutexes (for both actions and propositions)
+    '''
+
+
+    def __init__(self, a, b):
+        '''
+        Constructor
+        '''
+        self.a = a
+        self.b = b
+
+    def __eq__(self, other):
+        if (self.a == other.a) and (self.b == other.b):
+            return True
+        if (self.b == other.a) and (self.a == other.b):
+            return True
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __str__(self):
+        return str(self.a)+","+str(self.b)
+
+    def __repr__(self):
+        return "Pair("+ str(self) + ")"
+
+    def __hash__(self):
+        return hash((self.a,self.b))
 
 
 
